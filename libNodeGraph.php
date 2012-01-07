59318b6067a67dbedd4f75a34d91996d2c23a3f4
Nexii Malthus <nexiim@gmail.com>
2012-01-07 22:05:45 +0000

    Public functions

--- libNodeGraph.php
+++ libNodeGraph.php
@@ -1,26 +1,25 @@
 <?php
 // Released to Public Domain. Created by Nexii Malthus.
 
-class NodeGraph {
-	// This is a stub class.
-	// Meant to help clarify the methods (and why/for what) the Pathfinding lib
-	// will use when supplied with a node graph object.
-	// Note that $Node* should be an integer for array lookup purposes and use as references/pointers to nodes.
+interface NodeGraph {
+	// This is an interface of what is expected from a NodeGraph object.
+	// Comments help clarify why or for what the methods used in the Pathfinding lib.
+	// Note that $Node, $NodeFrom and $NodeTo should be an integer for array lookup purposes.
 	
 	
-	function Neighbours($Node) {
+	public function Neighbours($Node) {
 		// Return an array of connected neighbours.
 		return Array();
 	}
 	
-	function G($NodeFrom, $NodeTo) {
+	public function G($NodeFrom, $NodeTo) {
 		// We need the 'G' costs. The cost of choosing this node. (Choice costs? -- i.e. breaking down a door)
 		// You may interpret this as how difficult it would be to move to/in this node. (Movement costs?)
 		// As if it were a slope or other difficult terrain to manouver. (Terrain costs?)
 		return 0;
 	}
 	
-	function H($NodeFrom, $NodeTo) {
+	public function H($NodeFrom, $NodeTo) {
 		// We want the 'H' or heuristic cost between two nodes.
 		// You can interpret this as the euclidean distance.
 		// Using square roots isn't too bad and returns an extremely accurate result we need for a good path.
